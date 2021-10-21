da8dde0a18d91d801cde1d4b60f331bf984edeb5
theDanielTo <dto1989@gmail.com>
2021-10-21 14:06:31 -0700

    save google api key

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
+const GOOGLE_API_KEY = 'AIzaSyApCMsMiH2TBO2pZ6nRiHhCSzycQfEK3m8';
+
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
+
+  // send this to Google's API
 }
 
 form.addEventListener('submit', searchAddressHandler);
